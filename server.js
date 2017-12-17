c99f20281d32fe065f4c1a1a8b5a4bad3950ce5a
hamzaJridi <hamzajridi.h@gmail.com>
2017-12-17 19:59:07 +0100

    test heroku

--- server.js
+++ server.js
@@ -35,20 +35,20 @@ app.use(function (req, res, next) {
 })
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'src')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 // Set our api routes
 app.use('/api', api)
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000'
+const port = process.env.PORT || '8080'
 app.set('port', port)
 
 /**
