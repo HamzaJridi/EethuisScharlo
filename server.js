83fba3946f047ead0bfcd4a595dd0709ec0e99df
hamzaJridi <hamzajridi.h@gmail.com>
2017-12-17 19:50:18 +0100

    test herku

--- server.js
+++ server.js
@@ -15,24 +15,24 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Add headers
-// app.use(function (req, res, next) {
-//
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//   // Pass to next layer of middleware
-//   next();
-// })
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+})
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'src')))
@@ -41,9 +41,9 @@ app.use(express.static(path.join(__dirname, 'src')))
 app.use('/api', api)
 
 // Catch all other routes and return the index file
-// app.get('*', (req, res) => {
+app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
+});
 
 /**
  * Get port from environment and store in Express.
